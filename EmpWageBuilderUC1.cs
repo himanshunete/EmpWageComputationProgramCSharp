4b6f086b0cf0dfb4fa55f6627be5be32faae0573
himanshunete <himanshuneteh@gmail.com>
2021-02-02 15:36:36 +0530

    [himanshunete]Add CheckingAttendance

--- EmpWageBuilderUC1.cs
+++ EmpWageBuilderUC1.cs
@@ -12,10 +12,10 @@ namespace EmpWageComputationProgram
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             //Constant
-            int IS_FULL_TIME = 1;
+            int IS_PRESENT = 1;
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
             }
